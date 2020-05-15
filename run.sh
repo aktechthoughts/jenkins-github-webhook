ba64c106dee7573a8c2aad48a2af94f43f1c96d3
Add to docker group <ubuntu@ip-10-0-2-223.eu-central-1.compute.internal>
2020-05-15 10:58:20 +0000

    Added log

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 docker run  --name=jenkins_web -d \
 	-v /home/jenkins:/var/jenkins_home \
 	-p 8080:8080 \
-		aktechthoughts/aws-jenkins-docker:7
+		aktechthoughts/aws-jenkins-docker:7 \
+	--log-opt max-size=10m \
+	--log-opt max-file=5
+
 
